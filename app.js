98aa95bd4fff4a547764f4b128aba2a0d034d09e
Sakshi <sakshis.1407@gmail.com>
2020-10-12 00:36:22 +0530

    Added data persistence using Mongoose

--- app.js
+++ app.js
@@ -1,10 +1,37 @@
-var express = require("express");
-var app = express();
-var bodyParser = require("body-parser");
+var express     = require("express"),
+    app         = express(),
+    bodyParser  = require("body-parser"),
+    mongoose    = require("mongoose")
 
+mongoose.connect("mongodb://localhost/yelp_camp",{ useNewUrlParser: true, useUnifiedTopology: true });
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
+// SCHEMA SETUP
+var campgroundSchema = new mongoose.Schema({
+    name: String,
+    image: String,
+    description: String
+ });
+ 
+ var Campground = mongoose.model("Campground", campgroundSchema);
+ 
+ // Campground.create(
+ //      {
+ //          name: "Granite Hill", 
+ //          image: "https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg",
+ //          description: "This is a huge granite hill, no bathrooms.  No water. Beautiful granite!"
+          
+ //      },
+ //      function(err, campground){
+ //       if(err){
+ //           console.log(err);
+ //       } else {
+ //           console.log("NEWLY CREATED CAMPGROUND: ");
+ //           console.log(campground);
+ //       }
+ //     });
+ 
 var campgrounds = [
         {name: "Salmon Creek", image: "https://farm9.staticflickr.com/8442/7962474612_bf2baf67c0.jpg"},
         {name: "Granite Hill", image: "https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg"},
@@ -21,18 +48,34 @@ app.get("/", function(req, res){
     res.render("landing");
 });
 
+//INDEX - show all campgrounds
 app.get("/campgrounds", function(req, res){
-    res.render("campgrounds",{campgrounds:campgrounds});
+    // Get all campgrounds from DB
+    Campground.find({}, function(err, allCampgrounds){
+       if(err){
+           console.log(err);
+       } else {
+          res.render("campgrounds",{campgrounds:allCampgrounds});
+       }
+    });
 });
 
+//CREATE - add new campground to DB
 app.post("/campgrounds", function(req, res){
     // get data from form and add to campgrounds array
     var name = req.body.name;
     var image = req.body.image;
-    var newCampground = {name: name, image: image}
-    campgrounds.push(newCampground);
-    //redirect back to campgrounds page
-    res.redirect("/campgrounds");
+    var desc = req.body.description;
+    var newCampground = {name: name, image: image, description: desc}
+    // Create a new campground and save to DB
+    Campground.create(newCampground, function(err, newlyCreated){
+        if(err){
+            console.log(err);
+        } else {
+            //redirect back to campgrounds page
+            res.redirect("/campgrounds");
+        }
+    });
 });
 
 app.get("/campgrounds/new", function(req, res){
