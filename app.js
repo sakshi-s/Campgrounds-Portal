090d6be0eb6a7c7143da2fe0536321b9b6c3a0e4
Sakshi <sakshis.1407@gmail.com>
2020-11-18 15:37:13 +0530

    Added isLoggedIn middleware

--- app.js
+++ app.js
@@ -84,7 +84,7 @@ app.get("/campgrounds/:id", function(req, res){
 // COMMENTS ROUTES
 // ====================
 
-app.get("/campgrounds/:id/comments/new", function(req, res){
+app.get("/campgrounds/:id/comments/new", isLoggedIn, function(req, res){
     // find campground by id
     Campground.findById(req.params.id, function(err, campground){
         if(err){
@@ -95,7 +95,7 @@ app.get("/campgrounds/:id/comments/new", function(req, res){
     })
 });
 
-app.post("/campgrounds/:id/comments", function(req, res){
+app.post("/campgrounds/:id/comments", isLoggedIn, function(req, res){
    //lookup campground using ID
    Campground.findById(req.params.id, function(err, campground){
        if(err){
@@ -159,6 +159,13 @@ app.get("/logout", function(req, res){
     req.logout();
     res.redirect("/campgrounds");
  });
+
+function isLoggedIn(req, res, next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect("/login");
+}
 
 app.listen(3000, function(){
     console.log("The YelpCamp Server Has Started!");
