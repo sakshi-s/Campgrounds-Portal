6b10941aa9809b234eaa6ff63ebbcf5e42ab82c8
Sakshi <sakshis.1407@gmail.com>
2020-11-18 14:55:26 +0530

    Added login form

--- app.js
+++ app.js
@@ -141,8 +141,10 @@ app.post("/register", function(req, res){
     });
 });
 
-
-
+// show login form
+app.get("/login", function(req, res){
+    res.render("login"); 
+ });
 
 app.listen(3000, function(){
     console.log("The YelpCamp Server Has Started!");
