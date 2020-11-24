bbff163d77083847adf17f811cd4bbeccaf42197
Sakshi <sakshis.1407@gmail.com>
2020-11-24 19:09:03 +0530

    User campground association

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -20,7 +20,11 @@ router.post("/", isLoggedIn, function(req, res){
     var name = req.body.name;
     var image = req.body.image;
     var desc = req.body.description;
-    var newCampground = {name: name, image: image, description: desc}
+    var author = {
+        id: req.user._id,
+        username: req.user.username
+    }
+    var newCampground = {name: name, image: image, description: desc, author:author}
     // Create a new campground and save to DB
     Campground.create(newCampground, function(err, newlyCreated){
         if(err){
