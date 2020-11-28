07845c248605b8401b330bacc521bdc1de21fb6a
Sakshi <sakshis.1407@gmail.com>
2020-11-28 16:35:27 +0530

    Update comments

--- routes/comments.js
+++ routes/comments.js
@@ -67,6 +67,18 @@ router.put("/:id", function(req, res){
     });
 });
 
+// COMMENT DESTROY ROUTE
+router.delete("/:comment_id", function(req, res){
+    //findByIdAndRemove
+    Comment.findByIdAndRemove(req.params.comment_id, function(err){
+       if(err){
+           res.redirect("back");
+       } else {
+           res.redirect("/campgrounds/" + req.params.id);
+       }
+    });
+});
+
 //middleware
 function isLoggedIn(req, res, next){
     if(req.isAuthenticated()){
