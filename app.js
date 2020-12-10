c3d604db360449b4297091c76b727d750424785c
Sakshi <sakshis.1407@gmail.com>
2020-12-10 13:49:39 +0530

    add Mongo Atlas DB

--- app.js
+++ app.js
@@ -16,7 +16,8 @@ var commentRoutes    = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes      = require("./routes/index")
 
-mongoose.connect("mongodb://localhost/yelpcamp",{ useNewUrlParser: true, useUnifiedTopology: true });
+// mongoose.connect("mongodb://localhost/yelpcamp",{ useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect("mongodb://colt:rusty@ds055525.mongolab.com:55525/yelpcamp")
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
